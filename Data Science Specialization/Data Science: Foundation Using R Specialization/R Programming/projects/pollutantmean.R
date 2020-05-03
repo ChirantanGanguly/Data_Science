8796ce49d638f8bda911ef5015a64006757f2e9f
ChirantanGanguly <chirantanganguly01@gmail.com>
2020-05-03 16:30:44 +0530

    Update pollutantmean.R

--- Data Science Specialization/Data Science: Foundation Using R Specialization/R Programming/projects/pollutantmean.R
+++ Data Science Specialization/Data Science: Foundation Using R Specialization/R Programming/projects/pollutantmean.R
@@ -3,7 +3,7 @@ pollutantmean<-function(directory,pollutant,id){
     pmean<-c()
     for(i in id){
         filename<-paste(directory,"/",formatC(i,width=3,flag="0"),".csv",sep="")
-        x<-read.csv(filename)$sulfate
+        x<-read.csv(filename)$pollutant
         pmean<-c(pmean,mean(x[!is.na(x)]))
     }
     mean(pmean)
