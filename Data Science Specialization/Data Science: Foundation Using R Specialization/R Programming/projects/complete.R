7f7483a44079d41da678a36fa78d2d2851624e07
ChirantanGanguly <chirantanganguly01@gmail.com>
2020-05-03 19:33:36 +0530

    Update complete.R

--- Data Science Specialization/Data Science: Foundation Using R Specialization/R Programming/projects/complete.R
+++ Data Science Specialization/Data Science: Foundation Using R Specialization/R Programming/projects/complete.R
@@ -1,9 +1,23 @@
-complete<-function(directory,id){
-  num<-c()
+complete<-function(directory,id=1:332){
+  #Creating an empty vector nobs to store the number of completely observed cases in each data file.
+  nobs<-c()
+  
   for(i in id){
+    #creating the file name ("001.csv","002.csv","003.csv"..."332.csv")
     filename<-paste(directory,"/",formatC(i,width=3,flag="0"),".csv",sep="")
+    
+    #Reading the data in the .csv file and storing in x
     x<-read.csv(filename)
-    num<-c(num,min(length(x$Date[!is.na(x$Date)]),length(x$sulfate[!is.na(x$sulfate)]),length(x$nitrate[!is.na(x$nitrate)]),length(x$ID[!is.na(x$ID)])))
+    
+    #The number of completely observed cases is basically is the least length of all the different component vectors of
+    #the dataset x
+    k<-min(length(x$Date[!is.na(x$Date)]),length(x$sulfate[!is.na(x$sulfate)]),length(x$nitrate[!is.na(x$nitrate)]),
+           length(x$ID[!is.na(x$ID)]))
+    
+    #Concatenating the value of the present file to the vector nobs
+    nobs<-c(nobs,k)
   }
-  data.frame(id,num)
+  
+  #converting it into a data frame as we are required to give output as a data frame
+  data.frame(id,nobs)
 }
