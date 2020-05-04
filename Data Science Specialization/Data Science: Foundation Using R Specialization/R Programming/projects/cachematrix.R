adba4b77c183ae45089f2ec0c38ce49ef6d0775e
ChirantanGanguly <chirantanganguly01@gmail.com>
2020-05-04 09:33:50 +0530

    Add files via upload

---
+++ Data Science Specialization/Data Science: Foundation Using R Specialization/R Programming/projects/cachematrix.R
@@ -0,0 +1,31 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(matrix) {
+    x <<- matrix
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set, get = get,setinv = setinv, getinv = getinv)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i<-solve(data)
+  x$setinv(i)
+  i
+}
